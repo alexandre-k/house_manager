558fffee3052144df5efe366557d3b1cad59c2ab
Alexandre Krispin <k.m.alexandre@protonmail.com>
2022-08-13 09:05:38 +0900

    Update categories

--- src/utils/categories.ts
+++ src/utils/categories.ts
@@ -1,26 +1,41 @@
 // export const categories = ['食費', '遊び', '家賃', '光熱水費', '通信費', '日用品', '外食費'];
-export const expenditureTypes: Array<string> = ['food', 'entertainment', 'rent', 'electricity', 'communication', 'dailyNecessities', 'eatOutside']
+
+export const expenditureTypes: Array<string> = [
+    'food',
+    'entertainment',
+    'rent',
+    'electricity',
+    'communication',
+    'dailyNecessities',
+    'eatOutside',
+    'waterBill',
+    'gasBill'
+]
 
 export const expenditureColors: Array<string> = [
-    '#FFADAD',
-    '#FFD6A5',
-    '#FDFFB6',
-    '#CAFFBF',
-    '#9BF6FF',
-    '#A0C4FF',
-    '#FFC6FF'
+    '#FFADAD', // food
+    '#FFD6A5', // entertainment
+    '#FDFFB6', // rent
+    '#ffff00', // electricity
+    '#d3d9de', // communication
+    '#A0C4FF', // daily necessities
+    '#ff0066', // eat outside
+    '#00ffff', // water bill
+    '#800080'  // gas bill
 ]
 
 export const categoryColors: Record<string, string> = expenditureTypes.reduce((cc, cat, i) => ({ ...cc, [cat]: expenditureColors[i] }), {})
 
 export const expenditureHoverColors: Array<string> = [
-    '#FFC9C9',
-    '#FEE1C0',
-    '#FDFED2',
-    '#CAFFBF',
-    '#D7FDD0',
-    '#BED7FE',
-    '#FEDCFE'
+    '#FFC9C9', // food
+    '#FEE1C0', // entertainment
+    '#FDFED2', // rent
+    '#ffff99', // electricity
+    '#e2e6e9', // communication
+    '#BED7FE', // daily necessities
+    '#ff3385', // eat outside
+    '#99ffff', //  water bill
+    '#b300b3'  // gas bill
 ]
 
 export const categoryHoverColors: Record<string, string> = expenditureTypes.reduce((cc, cat, i) => ({ ...cc, [cat]: expenditureHoverColors[i] }), {})
