92db88e3a1d5d9cdfc96eb4078c2e63ae352742b
Alexandre Krispin <development.s004k@simplelogin.com>
2022-10-31 22:49:15 +0900

    Fix Dockerfile

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,2 @@
-docker build -t kmalexandre/uchinokakeibo:0.0.6 -f docker/Dockerfile.build .
-docker push kmalexandre/uchinokakeibo:0.0.6
+docker build -t kmalexandre/uchinokakeibo:0.0.7 -f docker/Dockerfile.build .
+docker push kmalexandre/uchinokakeibo:0.0.7
