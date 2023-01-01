730e5450a8365e3354b2a6e925fdf5cdab113ebf
Alexandre Krispin <development.s004k@simplelogin.com>
2023-01-01 18:06:44 +0900

    Shorten image name

--- src/components/NewReceipt.tsx
+++ src/components/NewReceipt.tsx
@@ -96,7 +96,7 @@ function NewReceipt({ date, setIsAddingReceipt }: INewReceiptProps) {
 
         const hash = arrayToHex(getHash(targetReceipt));
 
-        targetReceipt.imageName = hash + "." + imageName.split('.')[1];
+        targetReceipt.imageName = hash.substring(0, 6) + "." + imageName.split('.')[1];
         try {
 
             mutation.mutate({
